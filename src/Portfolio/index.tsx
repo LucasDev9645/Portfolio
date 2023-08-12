b0b40dcc6e98299def77d196d47c0edaad6902e2
LucasDev <LucasDev9645@gmail.com>
2023-08-11 22:37:49 -0300

    Create card project

--- src/Portfolio/index.tsx
+++ src/Portfolio/index.tsx
@@ -1,11 +1,89 @@
+import Tag from "../Tag";
+
+import imageTest from "../assets/img/disney+ 1.png";
+import imageSite from "../assets/img/pagina-da-internet 1.png";
+import imageGit from "../assets/img/icons8-github-24 1.png";
+
 import { Title } from "../styles/globalStyle";
-import { PortfolioContainer } from "./style";
+import { ContainerButton, PortfolioContainer } from "./style";
 
 const Portfolio = () => {
+  const projects = [
+    {
+      id: 1,
+      image: imageTest,
+      title: "Ecommerce Games",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea reprehenderit in voluptate velit esse cillum dolore.",
+    },
+    {
+      id: 2,
+      image: imageTest,
+      title: "Ecommerce Games",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea reprehenderit in voluptate velit esse cillum dolore.",
+    },
+    {
+      id: 3,
+      image: imageTest,
+      title: "Ecommerce Games",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea reprehenderit in voluptate velit esse cillum dolore.",
+    },
+    {
+      id: 4,
+      image: imageTest,
+      title: "Ecommerce Games",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea reprehenderit in voluptate velit esse cillum dolore.",
+    },
+    {
+      id: 5,
+      image: imageTest,
+      title: "Ecommerce Games",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea reprehenderit in voluptate velit esse cillum dolore.",
+    },
+    {
+      id: 6,
+      image: imageTest,
+      title: "Ecommerce Games",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea reprehenderit in voluptate velit esse cillum dolore.",
+    },
+  ];
+
   return (
-    <PortfolioContainer className="container">
-      <Title>Meus Projetos.</Title>
-    </PortfolioContainer>
+    <>
+      <PortfolioContainer className="container">
+        <Title>Meus Projetos.</Title>
+        <div>
+          <ul>
+            {projects.map((item) => (
+              <li key={item.id}>
+                <img src={item.image} alt="" />
+                <Tag>Html</Tag>
+                <Tag>Css</Tag>
+                <Tag>Javascript</Tag>
+                <Tag>Gulp</Tag>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <ContainerButton>
+                  <button>
+                    <img src={imageSite} alt="" />
+                    Visitar Site
+                  </button>
+                  <button>
+                    <img src={imageGit} alt="" />
+                    Ver Código
+                  </button>
+                </ContainerButton>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </PortfolioContainer>
+    </>
   );
 };
 
--- src/Portfolio/style.ts
+++ src/Portfolio/style.ts
@@ -1,9 +1,75 @@
 import { styled } from "styled-components";
 
 import { Title } from "../styles/globalStyle";
+import { TagContainer } from "../Tag/style";
 
 export const PortfolioContainer = styled.section`
   ${Title} {
     width: 14ch;
+  }
+
+  ${TagContainer} {
+    margin: 0.2rem;
+  }
+
+  div {
+    ul {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 2rem;
+
+      li {
+        padding: 0.6rem;
+        border-radius: 0.7rem;
+        width: 23.75rem;
+        height: 31.25rem;
+        background: #373737;
+        color: var(--color-gray-300);
+
+        img {
+          max-width: 100%;
+          pointer-events: none;
+        }
+
+        h3 {
+          margin-top: 1rem;
+          text-align: center;
+          font-size: 20px;
+        }
+
+        p {
+          margin-top: 0.5rem;
+          font-size: 0.85rem;
+          font-style: italic;
+        }
+      }
+    }
+  }
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 4rem;
+
+  button {
+    cursor: pointer;
+    border-radius: 0.5rem;
+    background-color: var(--color-blue-500);
+    box-shadow: 0 0 5px var(--color-blue-500), 0 0 25px var(--color-blue-500);
+    border: none;
+    width: 7rem;
+    height: 1.6rem;
+    font-weight: 700;
+    font-size: 0.8rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    text-transform: uppercase;
+
+    img {
+      width: 1.125rem;
+    }
   }
 `;
